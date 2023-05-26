d53c02e7f1f52eb0b9eaf33c33215fd14835858d
James Herrick <100632668+dudetechitout@users.noreply.github.com>
2023-05-26 11:34:10 -0400

    release

---
+++ pop-cat-bin-net/Form1.Designer.vb
@@ -0,0 +1,185 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.LinkLabel2 = New System.Windows.Forms.LinkLabel()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(22, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(410, 31)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Simply hover your mouse over which pop cat you'd like and click on it to have you" &
+    "r bin changed!"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.PictureBox2)
+        Me.GroupBox1.Controls.Add(Me.PictureBox4)
+        Me.GroupBox1.Controls.Add(Me.PictureBox5)
+        Me.GroupBox1.Controls.Add(Me.PictureBox3)
+        Me.GroupBox1.Controls.Add(Me.PictureBox6)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(457, 120)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Bin Icon Selection"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Image = Global.pop_cat_bin_net.My.Resources.Resources.dZn7WviI7yvdLdO7Leg6
+        Me.PictureBox1.Location = New System.Drawing.Point(108, 69)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(51, 33)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox2.Image = Global.pop_cat_bin_net.My.Resources.Resources.qrr3QW60WTrzf0Xcqlin
+        Me.PictureBox2.Location = New System.Drawing.Point(108, 69)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(51, 33)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox4.Image = Global.pop_cat_bin_net.My.Resources.Resources.uVn8uHXaUKANfhGFMpX4
+        Me.PictureBox4.Location = New System.Drawing.Point(212, 69)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(32, 33)
+        Me.PictureBox4.TabIndex = 7
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox5.Image = Global.pop_cat_bin_net.My.Resources.Resources.VUMcv6yiuUNl4hlcAH0I
+        Me.PictureBox5.Location = New System.Drawing.Point(212, 69)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(32, 33)
+        Me.PictureBox5.TabIndex = 8
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox3.Image = Global.pop_cat_bin_net.My.Resources.Resources.Wk43BkQjt48QmIBBVMhk
+        Me.PictureBox3.Location = New System.Drawing.Point(300, 69)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(51, 33)
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox6.Image = Global.pop_cat_bin_net.My.Resources.Resources.Nw6vUtDpPTTcc3WAR0um
+        Me.PictureBox6.Location = New System.Drawing.Point(300, 69)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(51, 33)
+        Me.PictureBox6.TabIndex = 9
+        Me.PictureBox6.TabStop = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(130, 138)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(126, 13)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Created by dudetechitout"
+        '
+        'LinkLabel2
+        '
+        Me.LinkLabel2.AutoSize = True
+        Me.LinkLabel2.Location = New System.Drawing.Point(262, 138)
+        Me.LinkLabel2.Name = "LinkLabel2"
+        Me.LinkLabel2.Size = New System.Drawing.Size(95, 13)
+        Me.LinkLabel2.TabIndex = 9
+        Me.LinkLabel2.TabStop = True
+        Me.LinkLabel2.Text = "Reset Recycle Bin"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 160)
+        Me.Controls.Add(Me.LinkLabel2)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Pop Cat Bin"
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents LinkLabel2 As LinkLabel
+End Class
---
+++ pop-cat-bin-net/Form1.vb
@@ -0,0 +1,118 @@
+﻿Imports Microsoft.Win32
+Imports System.IO
+Imports System.ComponentModel
+
+Public Class Form1
+    Dim icos_path As String = Environment.GetEnvironmentVariable("UserProfile") & "\popcat\"
+    Dim iconFileNames As String() = {
+            "atmIwDw04nJ3buvDDEXo.ico",
+            "e9tCRkrA0yoN4ddiJEwf.ico",
+            "Ks7NG6jZaTfeILNIVqx9.ico",
+            "Z9Ka9hceumSD7L8fBWHW.ico",
+            "Szs348wYMsMv0kEzRF2F.ico",
+            "ZHfTZbFjP4duJXeUluCE.ico"
+        }
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If Not System.IO.Directory.Exists(icos_path) Then
+            System.IO.Directory.CreateDirectory(icos_path)
+        End If
+
+        For Each icon_file_name As String In iconFileNames
+            If Not File.Exists(icos_path & icon_file_name) Then
+                Try
+                    Dim icon_name() As String = icon_file_name.Split(".")
+                    Dim icon_resource As Icon = DirectCast(My.Resources.ResourceManager.GetObject(icon_name(0)), Icon)
+
+                    Dim filePath As String = Path.Combine(icos_path, icon_file_name)
+
+                    Using fileStream As New FileStream(filePath, FileMode.Create)
+                        icon_resource.Save(fileStream)
+                    End Using
+
+                Catch ex As Exception
+                    MessageBox.Show("Unable to write resources to directory. Check permissions.")
+                End Try
+            End If
+        Next
+    End Sub
+
+    Private Sub SetRecycleBinIcon(ByVal name As String, ByVal iconIndex As Integer, ByVal iconFilePath As String)
+        Dim regKey As RegistryKey = Registry.CurrentUser.OpenSubKey("Software\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon", True)
+        regKey.SetValue(name, iconFilePath & "," & iconIndex)
+        regKey.Close()
+        NativeMethods.SHChangeNotify(&H8000000, &H1000, IntPtr.Zero, IntPtr.Zero)
+    End Sub
+
+
+    Private Sub PictureBox1_MouseHover(sender As Object, e As EventArgs) Handles PictureBox1.MouseHover
+        If PictureBox1.Visible Then
+            PictureBox1.Hide()
+        End If
+    End Sub
+
+    Private Sub PictureBox2_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox2.MouseLeave
+        If Not PictureBox1.Visible Then
+            PictureBox1.Show()
+        End If
+    End Sub
+
+    Private Sub PictureBox4_MouseHover(sender As Object, e As EventArgs) Handles PictureBox4.MouseHover
+        If PictureBox4.Visible Then
+            PictureBox4.Hide()
+        End If
+    End Sub
+
+    Private Sub PictureBox5_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox5.MouseLeave
+        If Not PictureBox4.Visible Then
+            PictureBox4.Show()
+        End If
+    End Sub
+
+    Private Sub PictureBox3_MouseHover(sender As Object, e As EventArgs) Handles PictureBox3.MouseHover
+        If PictureBox3.Visible Then
+            PictureBox3.Hide()
+        End If
+    End Sub
+
+    Private Sub PictureBox6_MouseLeave(sender As Object, e As EventArgs) Handles PictureBox6.MouseLeave
+        If Not PictureBox3.Visible Then
+            PictureBox3.Show()
+        End If
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        SetRecycleBinIcon("", 0, icos_path + iconFileNames(0))
+        SetRecycleBinIcon("empty", 0, icos_path + iconFileNames(0))
+        SetRecycleBinIcon("full", 0, icos_path + iconFileNames(2))
+    End Sub
+
+    Private Sub PictureBox5_Click(sender As Object, e As EventArgs) Handles PictureBox5.Click
+        SetRecycleBinIcon("", 0, icos_path + iconFileNames(5))
+        SetRecycleBinIcon("empty", 0, icos_path + iconFileNames(5))
+        SetRecycleBinIcon("full", 0, icos_path + iconFileNames(4))
+    End Sub
+
+    Private Sub PictureBox6_Click(sender As Object, e As EventArgs) Handles PictureBox6.Click
+        SetRecycleBinIcon("", 0, icos_path + iconFileNames(1))
+        SetRecycleBinIcon("empty", 0, icos_path + iconFileNames(1))
+        SetRecycleBinIcon("full", 0, icos_path + iconFileNames(3))
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://dudetechitout.com/")
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        SetRecycleBinIcon("", -55, "%SystemRoot%\System32\imageres.dll")
+        SetRecycleBinIcon("empty", -55, "%SystemRoot%\System32\imageres.dll")
+        SetRecycleBinIcon("full", -54, "%SystemRoot%\System32\imageres.dll")
+    End Sub
+End Class
+
+Public Class NativeMethods
+    <System.Runtime.InteropServices.DllImport("shell32.dll")>
+    Public Shared Sub SHChangeNotify(ByVal wEventId As Integer, ByVal uFlags As Integer, ByVal dwItem1 As IntPtr, ByVal dwItem2 As IntPtr)
+    End Sub
+End Class
